c5fea8f70ba895ff35cef08e84b2516cc12ad832
geallen <gamzesen3827@gmail.com>
2019-05-12 20:09:08 +0300

    Spring boot project from pluralsight

---
+++ das-boot/src/test/java/com/boot/ShipwreckControllerTest.java
@@ -0,0 +1,48 @@
+package com.boot;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.boot.controller.ShipwreckController;
+import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+
+public class ShipwreckControllerTest {
+
+	@InjectMocks
+	private ShipwreckController sc;
+	
+	@Mock
+	private ShipwreckRepository shipwreckRepo;
+	
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void testShipwreckGet() {
+		//ShipwreckController sc = new ShipwreckController();
+		
+		Shipwreck sw = new Shipwreck();
+		sw.setId(1l);
+		
+		when(shipwreckRepo.findOne(1l)).thenReturn(sw);
+		
+		
+		Shipwreck wreck = sc.get(1L);
+		//assertEquals(1l, wreck.getId().longValue());
+		
+		assertThat(wreck.getId(), is(1l));
+		
+	}
+	
+}
